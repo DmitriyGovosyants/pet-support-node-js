d2f27198ae6ab7db09043fc7a4c78a66fe2c6d2e
Maxim Stribylo <max057521@gmail.com>
2022-11-15 23:43:40 +0300

    update

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,18 +4,24 @@ const router = express.Router();
 
 module.exports = router;
 
-const { currentUserController } = require('../../controllers');
+const {
+  getPets,
+  getPetsById,
+  addPets,
+  deletePets,
+  currentUserController,
+} = require('../../controllers/userController');
 
 const { petsValidation, authValidation } = require('../../middlewares');
 const { authentificate } = require('../../middlewares/authentificate');
 const ctrlWrapper = require('../../helpers/ctrWrapper');
 
 router.get('/info', authentificate, ctrlWrapper()); // Роут отримання особистої інфо користувача
-router.get('/pets', authentificate, ctrlWrapper()); // Роут для отримання інфо о тваринах користувача
+router.get('/pets', authentificate, ctrlWrapper(getPets)); // Роут для отримання інфо о тваринах користувача
 router.patch('/info', authentificate, ctrlWrapper()); // Роут для отримання одного із полів інфо користувача
-router.post('/pets', authentificate, petsValidation, ctrlWrapper()); // Роут для додавання карточки тварини користувача авторизованим юзером
-router.delete('/pets/:petID', authentificate, ctrlWrapper()); // Роут для видалення карточки тварини користувача
-router.patch('/pets/:petID', authentificate, ctrlWrapper()); // Роут для редагування карточки тварин авторизованого користувача
+router.post('/pets', authentificate, petsValidation, ctrlWrapper(addPets)); // Роут для додавання карточки тварини користувача авторизованим юзером
+router.delete('/pets/:petID', authentificate, ctrlWrapper(deletePets)); // Роут для видалення карточки тварини користувача
+router.patch('/pets/:petID', authentificate, ctrlWrapper(getPetsById)); // Роут для редагування карточки тварин авторизованого користувача
 router.get(
   '/current',
   authentificate,
