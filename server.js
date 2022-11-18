bd44a8815b53324aad9c6d3ab1899378c238e6f8
Maxim Stribylo <max057521@gmail.com>
2022-11-19 01:53:52 +0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const { DB_HOST, PORT = 8080 } = process.env;
+const { DB_HOST, PORT = 5000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
