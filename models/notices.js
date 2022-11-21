ed9c319bd65591bdca55d75142546311527145a6
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-11-21 22:19:51 +0200

    feat: add sort in getPrivates and getFavorites

--- models/notices.js
+++ models/notices.js
@@ -1,52 +1,55 @@
 const { Schema, model } = require('mongoose');
 const Users = require('./users');
 
-const noticeSchema = new Schema({
-  category: {
-    type: String,
-    required: [true, 'Set category for notice'],
+const noticeSchema = new Schema(
+  {
+    category: {
+      type: String,
+      required: [true, 'Set category for notice'],
+    },
+    title: {
+      type: String,
+      required: [true, 'Set title for notice'],
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+    birthdate: {
+      type: String,
+      default: null,
+    },
+    breed: {
+      type: String,
+      default: null,
+    },
+    sex: {
+      type: String,
+      required: [true, 'Set sex for notice'],
+    },
+    location: {
+      type: String,
+      default: null,
+    },
+    price: {
+      type: String,
+      default: null,
+    },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
+    comments: {
+      type: String,
+      required: [true, 'Set comments for notice'],
+    },
+    owner: {
+      type: String,
+      ref: Users,
+    },
   },
-  title: {
-    type: String,
-    required: [true, 'Set title for notice'],
-  },
-  name: {
-    type: String,
-    default: null,
-  },
-  birthdate: {
-    type: String,
-    default: null,
-  },
-  breed: {
-    type: String,
-    default: null,
-  },
-  sex: {
-    type: String,
-    required: [true, 'Set sex for notice'],
-  },
-  location: {
-    type: String,
-    default: null,
-  },
-  price: {
-    type: String,
-    default: null,
-  },
-  avatarURL: {
-    type: String,
-    default: null,
-  },
-  comments: {
-    type: String,
-    required: [true, 'Set comments for notice'],
-  },
-  owner: {
-    type: String,
-    ref: Users,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Notice = model('notices', noticeSchema);
 
